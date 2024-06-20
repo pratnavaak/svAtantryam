c3dccd429adfcfd572a50e5d57a06521d067261d
ujjvlh <ujjwal.raajput@gmail.com>
2024-06-20 21:16:12 +0530

    a

--- work/population-density.py
+++ work/population-density.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import plotly.graph_objects as go
+import plotly.express as px
+import math
 
 file = [line.split(',') for line in open('population-density.csv').read().split('\n') if line][1:]
 
@@ -30,3 +33,33 @@ lbr = sorted([[c, name[c], before(c), latest(c), latest(c) / before(c)] for c in
 with open('lbr.csv','w+') as f:
     f.write(f'country code,country name,max population density before {ref} (or min of dataset if not available before {ref}),current population density,ratio\n')
     f.write('\n'.join([','.join([str(v) for v in c]) for c in lbr]))
+
+
+years = [str(y) for y in range(2018, 2024)]
+
+maxr = max([l[4] for l in lbr])
+tickvals = [1,10,100,1000,6000]
+
+fig = go.Figure(data=go.Choropleth(
+    locations = [l[0] for l in lbr],
+    z = [math.log(l[4]) for l in lbr],
+    text = [l[0] for l in lbr],
+        showscale=True,
+        colorscale = 'Turbo',
+        colorbar=dict(
+            tickvals = [math.log(v) for v in tickvals],
+            ticktext = tickvals,
+        ),
+        hovertext=[str(l[4]) for l in lbr],
+        marker_line_color='black',
+        marker_line_width=0.1,
+    ), layout=go.Layout(title = "industrial revolution effect: current population / max population before 1800"))
+
+fig.update_geos(
+    showcountries=True,
+    projection_type="miller"
+)
+
+fig.show()
+
+#fig.write_image('population-density-capacity.png')
